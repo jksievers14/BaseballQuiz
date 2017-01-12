6edb8cdd584f226e7c78b8bfef7530022fa3e831
jksievers14 <jksievers14@gmail.com>
2017-01-12 15:02:06 -0500

    Create QuizBaseball.py

---
+++ QuizBaseball.py
@@ -0,0 +1,193 @@
+# Trivia Quiz
+print("This is a quiz that tests your knwoledge of baseball")
+name = input("Enter your name:")
+
+print("\nHi, there", name + "Are you ready to take this quiz?\n")
+print("I will ask you 10 questions and give you three choices for each question")
+print("Select the right choice by pressing A, B or C")
+
+print("---------------------------------------------------------------------")
+
+# set quiz score to 0
+
+score = 0
+score = int(score) # convert 0 into a number so we can add scores
+
+# question 1
+
+print("QUESTION 1: How many players are there on a baseball diamond?")
+print("A. 6")
+print("B. 10")
+print("C. 9")
+Q1answer = "c"
+Q1response = input("your answer: ")
+if Q1response == "c" or Q1response == "C":
+    print("Good Job")
+    score = score+1
+
+else:
+    print("Too Bad")
+print("Your current score is " + str(score) + " out of 10")
+
+# question 2
+
+print("QUESTION 2: What is it called when you catch a ball in the air?")
+print("A. Fly out")
+print("B. Strike out")
+print("C.  Ground Out")
+Q1answer = "a"
+Q1response = input("your answer: ")
+if Q1response == "a" or Q1response == "A":
+    print("Good Job")
+    score = score+1
+
+else:
+    print("Too Bad")
+print("Your current score is " + str(score) + " out of 10")
+
+# question 3
+
+print("QUESTION 3: What are baseball officals called?")
+print("A. Referees")
+print("B. Rulers")
+print("C. Umpires")
+Q1answer = "c"
+Q1response = input("your answer: ")
+if Q1response == "c" or Q1response == "C":
+    print("Good Job")
+    score = score+1
+
+else:
+    print("Too Bad")
+print("Your current score is " + str(score) + " out of 10")
+
+# question 4
+
+print("QUESTION 4: Which of these players were caught using PEDs")
+print("A. Derek Jeter")
+print("B. Albert Pujols")
+print("C. Mark Mcguire")
+Q1answer = "c"
+Q1response = input("your answer: ")
+if Q1response == "c" or Q1response == "C":
+    print("Good Job")
+    score = score+1
+
+else:
+    print("Too Bad")
+print("Your current score is " + str(score) + " out of 10")
+
+# question 5
+
+print("QUESTION 5: Which team has the most world series trophies")
+print("A. Mets")
+print("B. Braves")
+print("C. Yankees")
+Q1answer = "c"
+Q1response = input("your answer: ")
+if Q1response == "c" or Q1response == "C":
+    print("Good Job")
+    score = score+1
+
+else:
+    print("Too Bad")
+print("Your current score is " + str(score) + " out of 10")
+
+# question 6
+
+print("QUESTION 6: What is it called when the ball clears the wall in fair territory?")
+print("A. Foul ball")
+print("B. Wild Pitch")
+print("C. Home Run")
+Q1answer = "c"
+Q1response = input("your answer: ")
+if Q1response == "c" or Q1response == "C":
+    print("Good Job")
+    score = score+1
+
+else:
+    print("Too Bad")
+print("Your current score is " + str(score) + " out of 10")
+
+# question 7
+
+print("QUESTION 7: Who is the all time leader in home runs?")
+print("A. Hank Aaron")
+print("B. Babe Ruth")
+print("C. Barry Bonds")
+Q1answer = "c"
+Q1response = input("your answer: ")
+if Q1response == "c" or Q1response == "C":
+    print("Good Job")
+    score = score+1
+
+else:
+    print("Too Bad")
+print("Your current score is " + str(score) + " out of 10")
+
+# question 8
+
+print("QUESTION 8: Who is the all time leader in stolen bases?")
+print("A. Hank Aaron")
+print("B. Babe Ruth")
+print("C. Ricky Henderson")
+Q1answer = "c"
+Q1response = input("your answer: ")
+if Q1response == "c" or Q1response == "C":
+    print("Good Job")
+    score = score+1
+
+else:
+    print("Too Bad")
+print("Your current score is " + str(score) + " out of 10")
+
+# question 9
+
+print("QUESTION 9: Who is the all time leader in hits?")
+print("A. Hank Aaron")
+print("B. Babe Ruth")
+print("C. Pete Rose")
+Q1answer = "c"
+Q1response = input("your answer: ")
+if Q1response == "c" or Q1response == "C":
+    print("Good Job")
+    score = score+1
+
+else:
+    print("Too Bad")
+print("Your current score is " + str(score) + " out of 10")
+
+# question 10
+
+print("QUESTION 10: Who is the all time leader in hits in a season?")
+print("A. Hank Aaron")
+print("B. Babe Ruth")
+print("C. Ichiro")
+Q1answer = "c"
+Q1response = input("your answer: ")
+if Q1response == "c" or Q1response == "C":
+    print("Good Job")
+    score = score+1
+
+else:
+    print("Too Bad")
+print("Your current score is " + str(score) + " out of 10")
+
+
+
+      
+      
+      
+
+
+
+
+
+
+
+
+
+    
+
+
+
